d73db8e40f1fd4e5bbde92dd6c688240e652a800
Kent Primrose <krose@solsoft.net>
2017-05-17 21:36:39 -0500

    Sample with supplied middleware (cors)

--- example/server.js
+++ example/server.js
@@ -3,6 +3,7 @@ const micdrop = require('..');
 module.exports = micdrop.init({
   PORT: 3500,
   middleware: [
+    require('cors')(),
     require('./middleware/test_middleware')
   ],
   routes: [{
